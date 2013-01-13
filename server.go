9e3f2e787e39d85c133b91cc94b4662746c32435
Nicolas Drebenstedt <nd026@hdm-stuttgart.de>
2013-01-13 16:44:40 +0100

    Get trending topics from Wiki

--- server.go
+++ server.go
@@ -1,14 +1,51 @@
-package hello
+package zeitnow
 
 import (
-    "fmt"
-    "net/http"
+	"encoding/xml"
+	"fmt"
+	"net/http"
+	"appengine"
+	"appengine/urlfetch"
+	"io/ioutil"
 )
 
+func wikiWords(w http.ResponseWriter, r *http.Request) {
+	type Page struct {
+		Title	string	`xml:"title"`
+	}
+	
+	type Result struct {
+		XMLName	xml.Name `xml:"pages"`
+		Type	string	 `xml:"type,attr"`
+		Page	[]Page	 `xml:"page"`
+	}
+
+	c := appengine.NewContext(r)
+	client := urlfetch.Client(c)
+
+	resp, err := client.Get("http://www.trendingtopics.org/pages.xml")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	v := Result{}
+	err = xml.Unmarshal([]byte(body), &v)
+
+	if err != nil {
+		fmt.Fprintf(w, "No marsh: %v", err)
+	}
+	fmt.Fprintf(w, "%v", v)
+	return
+}
+
 func init() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+	wikiWords(w, r)
 }
